fea8a8b88bdd3d37ba328328709e5fc5a08a2275
tamara <hronsky.pavol@gmail.com>
2017-01-26 14:32:24 +0100

    some classes added

---
+++ challengr/src/java/Challange.java
@@ -0,0 +1,11 @@
+/**
+ * Created by tamara on 26/01/2017.
+ */
+public class Challange {
+
+  private Long id;
+
+  private String description;
+
+  private String boilerPlateCode;
+}
--- challengr/src/java/User.java
+++ challengr/src/java/User.java
@@ -1,3 +1,15 @@
+
+
+/**
+ * Created by tamara on 26/01/2017.
+ */
 public class User {
 
+  private Long id;
+
+  private String email;
+
+  private String name;
+
+  private Integer priviledge;
 }
