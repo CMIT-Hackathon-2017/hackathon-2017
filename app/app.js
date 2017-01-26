4692a034d133ff9fda58db49169751401b99495e
Nemo Petersson <nemo.petersson@nordea.com>
2017-01-26 16:43:24 +0100

    add foundations grid to project. make challengeOverview page with challengeList component. Mocked test data

--- app/app.js
+++ app/app.js
@@ -2,12 +2,17 @@
 
 // Declare app level module which depends on views, and components
 angular.module('challengR', [
-  'ngRoute',
-  'challengR.login.component',
-  'challengR.login.controller',
-  'challengR.loginPage.controller'
+    'ngRoute',
+    'challengR.login.component',
+    'challengR.login.controller',
+    'challengR.loginPage.controller',
+    'challengR.challengeOverviewPage.controller',
+    'challengR.challengeList.controller',
+    'challengR.challengeList.component'
 
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $routeProvider.otherwise({redirectTo: '/login'});
 }]);
+
+//$(document).foundation();
