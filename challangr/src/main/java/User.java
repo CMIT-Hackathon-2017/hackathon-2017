29d6055bccb4ef5383fd32b87c10cf011717981d
Per Ahlbom <Per.Ahlbom@nordea.com>
2017-01-26 22:36:01 +0100

    fjdijpof

--- challangr/src/main/java/User.java
+++ challangr/src/main/java/User.java
@@ -1,8 +1,4 @@
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 /**
  * Created by tamara on 26/01/2017.
