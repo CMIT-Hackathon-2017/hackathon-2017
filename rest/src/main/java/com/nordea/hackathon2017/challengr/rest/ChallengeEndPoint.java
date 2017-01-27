2c922d3538bd1c00b6e9c6e2f6c835caa851ba5a
tamara <hronsky.pavol@gmail.com>
2017-01-27 12:54:49 +0100

    save response.data to sessionStorage

--- rest/src/main/java/com/nordea/hackathon2017/challengr/rest/ChallengeEndPoint.java
+++ rest/src/main/java/com/nordea/hackathon2017/challengr/rest/ChallengeEndPoint.java
@@ -3,14 +3,15 @@ package com.nordea.hackathon2017.challengr.rest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nordea.hackathon2017.pojo.Challenge;
-import com.nordea.hackathon2017.pojo.User;
+
 import com.nordea.hackathon2017.utils.DatabaseUtil;
-import javafx.scene.chart.PieChart;
+
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
+
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.sql.SQLException;
@@ -27,12 +28,10 @@ public class ChallengeEndPoint {
   @GET
   @Path("/get")
   @Consumes(MediaType.APPLICATION_JSON)
-  public Response createUserInJSON() throws SQLException {
-    //send user to database check if ok return id error handling
+  public Response getListChallengeInJSON() throws SQLException {
+//send user to database check if ok return id error handling
     DatabaseUtil.makeConnection();
     List<Challenge> challengeList = DatabaseUtil.selectAllChallenges();
-
-
     String result = null;
     try {
       result = challengeList.isEmpty() ? "{}" : mapper.writeValueAsString(challengeList);
@@ -40,6 +39,15 @@ public class ChallengeEndPoint {
       e.printStackTrace();
       result = "{}";
     }
-    return Response.status(201).entity(result).build();
+    return Response.status(200).entity(result).build();
+  }
+
+  @GET
+  @Path("/get/{id}")
+  public Response getChallengeInJSON(@PathParam("id") int id) throws SQLException{
+    DatabaseUtil.makeConnection();
+    Challenge challengeResp = DatabaseUtil.selectFromChallenges(id);
+    String result = challengeResp == null? "{}":challengeResp.toString();
+    return Response.status(200).entity(result).build();
   }
 }
